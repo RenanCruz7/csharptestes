a58aa01fb5975f2c83f729773e7ffcf7d58b7ed5
RenanCruz7 <renancruz199@gmail.com>
2025-07-03 21:08:20 -0300

    Feat: Testando oferta com rota nula

--- tests/JornadaMilhas.Test/OfertaViagemTest.cs
+++ tests/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -16,5 +16,18 @@ namespace JornadaMilhas.Test
             
             Assert.Equal(validacao, oferta.EhValido);
         }
+
+        [Fact]
+        public void TestandoOfertaComRotaNula()
+        {
+            Rota rota = null;
+            Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+            double preco = 100.0;
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
+            Assert.False(oferta.EhValido);
+        }
     }
 }
