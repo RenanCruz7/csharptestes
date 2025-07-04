6021ce77e642131d20554be3eb1e64cf47b8ca5a
RenanCruz7 <renancruz199@gmail.com>
2025-07-03 21:04:56 -0300

    Feat: Criando primeiro teste

---
+++ tests/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -0,0 +1,20 @@
+using JornadaMilhasV1.Modelos;
+
+namespace JornadaMilhas.Test
+{
+    public class OfertaViagemTest
+    {
+        [Fact]
+        public void TestandoOfertaValida()
+        {
+            Rota rota = new Rota("São Paulo", "Rio de Janeiro");
+            Periodo periodo = new Periodo(new DateTime(2023, 10, 1), new DateTime(2023, 10, 10));
+            double preco = 100.0;
+            var validacao = true;
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+            
+            Assert.Equal(validacao, oferta.EhValido);
+        }
+    }
+}
