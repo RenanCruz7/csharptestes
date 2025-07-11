217c68237198076e36bab390c22c6c76a193ea26
RenanCruz7 <renancruz199@gmail.com>
2025-07-10 21:59:12 -0300

    Feat: Aprendendo sobre testes de mutação

--- tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
+++ tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
@@ -26,20 +26,19 @@ namespace JornadaMilhas.Test
             Assert.Equal(precoComDesconto, oferta.Preco);
         }
 
-        [Fact]
-        public void RetornaDescontoMaximoQuandoValorDescontoMaiorQuePreco()
+        [Theory]
+        [InlineData(120,30)]
+        [InlineData(100, 30)]
+        public void RetornaDescontoMaximoQuandoValorDescontoMaiorOuIgualAoPreco(double desconto, double precoComDesconto)
         {
 
             Rota rota = new Rota("São Paulo", "Rio de Janeiro");
             Periodo periodo = new Periodo(new DateTime(2023, 10, 1), new DateTime(2023, 10, 10));
             double precoOriginal = 100.00;
-            double desconto = 120.00;
-            double precoComDesconto = 30.00;
 
             OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
 
             oferta.Desconto = desconto;
-
 
             Assert.Equal(precoComDesconto, oferta.Preco);
         }
