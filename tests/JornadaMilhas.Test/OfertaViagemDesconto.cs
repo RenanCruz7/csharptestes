b6dd717ae587f099f81c971d09f853f750729572
RenanCruz7 <renancruz199@gmail.com>
2025-07-10 21:48:07 -0300

    Feat: Aumentando a cobertura de testes

--- tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
+++ tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
@@ -43,5 +43,21 @@ namespace JornadaMilhas.Test
 
             Assert.Equal(precoComDesconto, oferta.Preco);
         }
+
+        [Fact]
+        public void RetornaTresErrosDeValidacaoQuandoRotaPeriodoEPRecoSaoInvalidos()
+        {
+            //arrange
+            int quantidadeEsperada = 3;
+            Rota rota = null;
+            Periodo periodo = new Periodo(new DateTime(2024, 6, 1), new DateTime(2024, 5, 10));
+            double preco = -100;
+
+            //act
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            //assert
+            Assert.Equal(quantidadeEsperada, oferta.Erros.Count());
+        }
     }
 }
